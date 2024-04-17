2bcb51c760debe7c7158c0e4b0c7e2b4b092fc5f
Nayem Islam <53568454+nayem-miah@users.noreply.github.com>
2024-04-17 17:56:46 +0600

    'commited'

--- app/page.js
+++ app/page.js
@@ -1,28 +1,13 @@
-import getJoke from "@/utils/getJoke";
-import dynamic from "next/dynamic";
-import { cookies } from 'next/headers'
-import RandomJoke from "./components/RandomJoke";
-import RandomJokeClient from "./components/RandomJokeClient";
 
+import NewUserForm from "./components/NewUserForm";
+import UserList from "./components/UserList";
 
-export const revalidate = 10;
 export default async function Home() {
-
-// // the 2 line code used to dynamic the 
-// // fetch otherwise it is fetched and cached
-//   const cookieStore = cookies()
-//   const theme = cookieStore.get('theme')
-
-
-  const joke = await getJoke();
-  return (
-    <main className="flex min-h-screen flex-col items-center 
-    justify-between p-24 gap-5">
- 
-       {joke.value}
-       <RandomJoke/>
-       <RandomJokeClient/>
-
-    </main>
-  );
+    return (
+        <div>
+            <h1 className="text-gray-800">USER REGISTRATION</h1>
+            <NewUserForm />
+            <UserList />
+        </div>
+    );
 }
